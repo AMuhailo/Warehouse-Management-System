80ba89efbb1f6dcdce83c43e3826b729c56624b7
amuhailo25@gmail.com <154220955+AMuhailo@users.noreply.github.com>
2025-03-19 16:36:19 +0200

    🟣Docker Images WorkFlows

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -32,11 +32,12 @@ SECRET_KEY = env('SECRET_KEY', default = 'your_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONMENT == 'local':
     DEBUG = True
+    ALLOWED_HOSTS = []
 else:
     DEBUG = False
-    
-ALLOWED_HOSTS = ['127.0.0.1','localhost','warehouse-management.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://warehouse-management.up.railway.app']
+    ALLOWED_HOSTS = ["warehouse-management.up.railway.app", "127.0.0.1", "localhost"]
+    CSRF_TRUSTED_ORIGINS = ["https://warehouse-management.up.railway.app"]
+
 
 
 # Application definition
